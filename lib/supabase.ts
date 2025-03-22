f08f4127e1b70090772a3bf80acb12c33737b80a
braile <braile.c.gawigawen@gmail.com>
2025-03-22 10:02:44 +0800

    [Auth] init auth commit

---
+++ lib/supabase.ts
@@ -0,0 +1,30 @@
+import { AppState } from "react-native";
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://segaekevskqjlmbrgcqg.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlZ2Fla2V2c2txamxtYnJnY3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NzM1NDIsImV4cCI6MjA1NDA0OTU0Mn0.gx_aibvRAelQu__S8wO2C4S0qAJfMnU1u-_ypfDfVaE";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
